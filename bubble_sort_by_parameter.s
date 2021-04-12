b75416a75c25f3dee3f73441f3c7083f9cb151de
anitatabaczka <anitatabacka@gmail.com>
2021-04-12 19:57:34 -0400

    array bubble sort by parameter

---
+++ bubble_sort_by_parameter.s
@@ -0,0 +1,36 @@
+.global _start
+.text
+.data
+tab: .long 9,1,8,2,7,3,6,4,5
+e:
+.equ s, (e-tab)/4
+f:
+mov $0, %ESI
+mov $0, %EDX
+p:
+cmp %ECX, %ESI
+jge r
+mov (%EBX,%ESI,4), %EAX
+inc %ESI
+cmp %EAX, (%EBX,%ESI,4)
+jl x
+jmp p
+x:
+xchg %EAX, (%EBX,%ESI,4)
+dec %ESI
+mov %EAX, (%EBX, %ESI, 4)
+inc %ESI
+mov $1, %EDX
+jmp p
+r:
+cmp $1, %EDX
+je f
+ret
+
+_start:
+push %ECX
+push %EBX
+mov $tab, %EBX
+mov $s, %ECX
+call f
+nop

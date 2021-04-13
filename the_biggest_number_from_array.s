dbd9834422a345e71867eb853860bc70ab5516e3
anitatabaczka <anitatabacka@gmail.com>
2021-04-12 20:11:05 -0400

    EDX = number, ESI = index

---
+++ the_biggest_number_from_array.s
@@ -0,0 +1,26 @@
+.global _start
+.text
+.data
+tab: .long 1,7,9,5,2
+e:
+.equ s, (e-tab)/4
+_start:
+mov $0, %EAX
+mov $0, %EBX
+mov $s, %ECX
+w:
+cmp %EAX, %ECX
+je end
+inc %EAX
+cmp tab(%EBX), %EDX
+jl x
+add $4, %EBX
+jmp w
+x:
+mov tab(%EBX), %EDX
+mov %EAX, %ESI
+add $4, %EBX
+jmp w
+end:
+nop
+#EDX = value, ESI = index
